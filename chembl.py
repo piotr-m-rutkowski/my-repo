255dc7d1b866fe7a267c5d6127b517f7a8a0d8a7
Piotr Rutkowski <159493435+piotr-m-rutkowski@users.noreply.github.com>
2024-02-22 09:43:33 +0000

    Update chembl.py

--- chembl.py
+++ chembl.py
@@ -1,6 +1,22 @@
-import pandas as pd
+## CHEMBL API - type retrieval from ID
 import requests
+import json
 
+#requests.get(url, params={key: value}, args)
+id = input('Input the number part of Chembl ID, e.g. 10 for CHEMBL10')
+r = requests.get('https://www.ebi.ac.uk/chembl/api/data/chembl_id_lookup/CHEMBL'+id+'.json')
+print(r.status_code)
+print('###')
+#print(r.json())
+print(r.json()['entity_type'])
 
+#data = r.json()
 
-print("hello")
+## ASSAY API 
+## https://www.ebi.ac.uk/chembl/api/data/assay/CHEMBL1002069.json
+
+## COMPOUND API 
+## https://www.ebi.ac.uk/chembl/api/data/molecule/CHEMBL10.json
+
+## TARGET API 
+## https://www.ebi.ac.uk/chembl/api/data/target/CHEMBL1075138.json
